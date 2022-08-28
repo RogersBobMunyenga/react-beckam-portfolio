836ac8e26a247389222a4fcd39dababa7e3fe8e3
RogersBobMunyenga <b.munyenga@gmail.com>
2022-08-28 14:48:21 +0300

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Pages from "./components/pages/Pages";
+
 
 function App() {
   return (
     <>
-    <Navbar/>
+    <Pages/>
+    
     </>
 
   );
