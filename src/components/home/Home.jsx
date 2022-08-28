853a3e4b9387a0ce0c3fd68c861d07865a81fe3a
RogersBobMunyenga <b.munyenga@gmail.com>
2022-08-28 15:47:26 +0300

    about section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Hero } from '../home/Hero'
+import { About } from '../pages/About'
 
 
 const Home = () => {
   return (
     <>
     <Hero />
+    <About />
     </>
   )
 }
